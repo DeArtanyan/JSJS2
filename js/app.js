0364fe0bc8fc7d1457c786e371e6459f36c0f27a
DeArtanyan <stretovich200@gmail.com>
2025-02-10 08:46:17 +0700

    twoth commit

--- js/app.js
+++ js/app.js
@@ -36,6 +36,19 @@ new Vue({
                 });
                 card.newItem = '';
             }
+        },
+        checkCardProgress(card, columnIndex) {
+            if (columnIndex === 0) {
+                const totalItems = card.items.length;
+                const completedItems = card.items.filter(item => item.completed).length;
+                if (completedItems > totalItems / 2) {
+                    this.moveCardToSecondColumn(card);
+                }
+            }
+        },
+        moveCardToSecondColumn(card) {
+            this.columns[0].cards = this.columns[0].cards.filter(c => c !== card);
+            this.columns[1].cards.push(card);
         }
     }
 });
